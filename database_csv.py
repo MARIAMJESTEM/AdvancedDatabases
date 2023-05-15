6af0f476a2a0247173f1161514fcbb2658800ec9
Filip <filipgaciarz@gmail.com>
2023-05-16 00:35:54 +0200

    category

--- database_csv.py
+++ database_csv.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine,MetaData, Table
+from faker import Faker
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 import database_architecture
 
-db_string = "postgresql://postgres:postgres@localhost:5432/advanced_databases"
+db_string = "postgresql://postgres:postgres@localhost:5433/advanced_databases"
 engine = create_engine(db_string)
 
 usecols = ["id","category_id","title", "author", "description", "release_year"]
 df = pd.read_csv('books_v2.csv', usecols=usecols)
 category = pd.unique(df['category_id'])
-map_list = {a : b for a,b in zip(category,range(1,len(category)))} # map category
+map_list = {a : b for a,b in zip(category,range(1,len(category) + 1))} # map category
 df['category_id'] = df['category_id'].map(map_list)
+# print(df.info())
 
-df.to_sql('book', con = engine, if_exists='replace', index=False)
+df2 = pd.read+csv('category.csv')
 
-
-# df.to_sql(name = df, con = engine, if_exists = 'replace', index = False)
+# df.to_sql('book', con = engine, if_exists='replace', index=False)
+# df2.to_sql('book_category', con = engine, if_exists='replace', index=False)
 
 
 
