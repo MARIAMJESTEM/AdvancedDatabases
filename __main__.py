02965c0db67dc32b0aef07252339bd78d2f6060f
MARIAMJESTEM <mariamazur.2000@wp.pl>
2023-05-16 07:18:53 +0200

    Done TODO's and added negative search

--- __main__.py
+++ __main__.py
@@ -4,11 +4,12 @@ from typing import List, Any, cast
 from sqlalchemy import create_engine
 from database_queries import DatabaseQueries
 from flask import Flask, render_template, request, flash, redirect, url_for
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '27ba51b46332c22d3005b6534d881908'
 LoggedIn = False
+usernameDB = ""
+search_term = ""
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -26,10 +27,12 @@ def home():
 def login():
     if request.method == 'POST':
         global LoggedIn
+        global usernameDB
         username = request.form['username']
         password = request.form['password']
         LoggedIn = query.check_user_password(username=username, password=password)
         if LoggedIn:
+            usernameDB = username
             return redirect(url_for('home'))
         else:
             flash('Given username or password is incorrect')
@@ -41,6 +44,7 @@ def login():
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
     global LoggedIn
+    global usernameDB
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -49,6 +53,7 @@ def signin():
         isEmail = not query.check_is_email_taken(email=email)
         if isUserName and isEmail:
             LoggedIn = True
+            usernameDB = username
             query.add_new_user_to_database(username=username, password=password, email=email)
             return redirect(url_for('home'))
         elif not isUserName:
@@ -63,8 +68,8 @@ def signin():
 @app.route("/userbooks", methods=['GET', 'POST'])
 def userbooks():
     global LoggedIn
-    # TODO Add a 'username' parameter to distinguish the user. | @MARIAMJESTEM
-    data = query.get_user_read_books(username=username)
+    global usernameDB
+    data = query.get_user_read_books(username=usernameDB)
     columns = ["title", "author", "genre", "release_year"]
 
     if request.method == 'POST':
@@ -73,20 +78,20 @@ def userbooks():
             return redirect(url_for('home'))
         title = request.form['title']
 
-        isTitleInDB = query.check_is_title_in_user_database(username=username, book_title=title)
+        isTitleInDB = query.check_is_title_in_user_database(username=usernameDB, book_title=title)
         if isTitleInDB:
             flash('Given book title is already added to your list!')
             return redirect(url_for('userbooks'))
         else:
-            query.add_read_book_to_user_list(username=username, book_title=title)
+            query.add_read_book_to_user_list(username=usernameDB, book_title=title)
     return render_template('userBooks.html', LoggedIn=LoggedIn, data=data, columns=columns)
 
 
 @app.route("/userreviews", methods=['GET', 'POST'])
 def userreview():
     global LoggedIn
-    # TODO Add a 'username' parameter to distinguish the user. | @MARIAMJESTEM
-    data = query.get_user_reviews(username=username)
+    global usernameDB
+    data = query.get_user_reviews(username=usernameDB)
     columns = ["title", "rating", "comment"]
 
     if request.method == 'POST':
@@ -95,31 +100,42 @@ def userreview():
             return redirect(url_for('home'))
         title = request.form['title']
         review = request.form['description']
-        # TODO Add a 'rating' and map values to float. Values between 0 and 5. | @MARIAMJESTEM
+        rating = float(request.form['quantity'])
 
         isTitleInDB = False  # Here assign the response from data base
         if isTitleInDB:
             flash('Given book title is already commented!')
             return redirect(url_for('userreview'))
         else:
-            query.add_review_to_user_book(username=username, book_title=title, rating=rating, comment=review)
+            query.add_review_to_user_book(username=usernameDB, book_title=title, rating=rating, comment=review)
     return render_template('userReviews.html', LoggedIn=LoggedIn, data=data, columns=columns)
 
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
     global LoggedIn
+    global search_term
     if request.method == 'POST':
         if "action" in request.form and request.form["action"] == "LogOut":
-            print("hi")
             LoggedIn = False
             return redirect(url_for('search'))
         search_term = request.form['search']
-
-        bookData = query.get_search_book_by_title(book_title=search_term)
+        try:
+            bookData = query.get_search_book_by_title(book_title=search_term)
+        except Exception as e:
+           return render_template('search_negative.html', LoggedIn=LoggedIn, search=search_term) 
         reviews = query.get_book_reviews(book_title=search_term)
     return render_template('search.html', LoggedIn=LoggedIn, Reviews=reviews, bookData=bookData)
 
+@app.route("/search_negative", methods=['GET', 'POST'])
+def search_negative():
+    global LoggedIn
+    global search_term
+    if request.method == 'POST':
+        if "action" in request.form and request.form["action"] == "LogOut":
+            LoggedIn = False
+            return redirect(url_for('search_negative'))
+    return render_template('search_negative.html', LoggedIn=LoggedIn, search=search_term)
 
 if __name__ == '__main__':
     db_string = "postgresql://postgres:postgres@localhost:5432/advanced_databases"
