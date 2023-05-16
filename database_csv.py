4fb1d15125ca72e9080aef7f41be9dff4d17a022
Bartosz Sroka <srokab019@gmail.com>
2023-05-16 13:01:04 +0200

    Fix the code that adds data to the database

--- database_csv.py
+++ database_csv.py
@@ -3,26 +3,37 @@ import numpy as np
 from faker import Faker
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 import database_architecture
-
-db_string = "postgresql://postgres:postgres@localhost:5433/advanced_databases"
-engine = create_engine(db_string)
-
-usecols = ["id","category_id","title", "author", "description", "release_year"]
-df = pd.read_csv('books_v2.csv', usecols=usecols)
-category = pd.unique(df['category_id'])
-map_list = {a : b for a,b in zip(category,range(1,len(category) + 1))} # map category
-df['category_id'] = df['category_id'].map(map_list)
-# print(df.info())
-
-df2 = pd.read_csv('category.csv')
-
-df.id = df.id.astype('int64')
-df.release_year = df.release_year.astype('int64')
-df.to_sql('book', con = engine, if_exists='append', index=False)
-df2.to_sql('book_category', con = engine, if_exists='append', index=False)
+from sqlalchemy.engine import Engine
 
 
+class DatabaseUpload:
+    def __init__(self, engine: Engine, book_data_csv: str, category_data_csv: str):
+        self.engine = engine
+        self.book_df = pd.read_csv(book_data_csv,
+                                   usecols=["id", "category_id", "title", "author", "description", "release_year"])
+        self.category_df = pd.read_csv(category_data_csv)
+
+    def upload_data(self):
+        self.upload_category_data()
+        self.upload_books_data()
+
+    def upload_books_data(self):
+        category = pd.unique(self.book_df['category_id'])
+        map_list = {a: b for a, b in zip(category, range(1, len(category) + 1))}
+        self.book_df['category_id'] = self.book_df['category_id'].map(map_list)
+
+        self.book_df.id = self.book_df.id.astype('int64')
+        self.book_df.release_year = self.book_df.release_year.astype('int64')
+        self.book_df.to_sql('book', con=engine, if_exists='append', index=False)
+
+    def upload_category_data(self):
+        self.category_df['name'] = self.category_df['name'].apply(lambda x: x[:20])
+        self.category_df['description'] = self.category_df['description'].apply(lambda x: x[:200])
+        self.category_df.to_sql('book_category', con=self.engine, if_exists='append', index=False)
 
 
-
-
+if __name__ == "__main__":
+    db_string = "postgresql://postgres:postgres@localhost:5432/advanced_databases"
+    engine = create_engine(db_string)
+    DU = DatabaseUpload(engine, 'books_v2.csv', 'category.csv')
+    DU.upload_data()
